ad0f6a50b999fb8d87db27cf28259519b014c387
Mushlihs <mushlih.sci@gmail.com>
2023-09-18 07:27:26 +0700

    first commit

---
+++ resources/views/components/dash-nav.blade.php
@@ -0,0 +1,8 @@
+<div class="w-full text-white d-flex position-absolute top-0 px-3 py-2 justify-content-end align-items-baseline z-3 ">
+    <ul class="d-flex gap-4 nolist">
+        <li><a class="text-white opacity-75 " href="{{URL("/")}}">Home</a></li>
+        <li><a class="text-white opacity-75 " href="#product">Product</a></li>
+        <li><a class="text-white opacity-75 " href="#about">About</a></li>
+    </ul>
+    
+</div>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WebController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [WebController::class,"home"]);
+Route::get('/post-{id}', [WebController::class,"detail"]);
+Route::get('/page', [WebController::class,"page"]);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
